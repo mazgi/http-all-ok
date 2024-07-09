4df3740216b27d919687b49698ee89281b2b16f0
user <user@MacBook-Pro-A927.local>
2024-07-09 17:10:37 +0900

    show app version and information

--- workspace/http-ok-whatever/main.go
+++ workspace/http-ok-whatever/main.go
@@ -8,15 +8,32 @@ import (
 	"os"
 )
 
+var appVersion = "2006.1.2"
+var appGitShortSha = "undefined"
+
+type AppInfo struct {
+	Version string `json:"version"`
+}
+
 type Response struct {
-	Method string `json:"method"`
-	Path   string `json:"path"`
+	AppInfo AppInfo `json:"app_info"`
+	Method  string  `json:"method"`
+	Path    string  `json:"path"`
+}
+
+func appInfo() (appInfo AppInfo) {
+	var version = fmt.Sprintf("%s+%s", appVersion, appGitShortSha)
+	appInfo = AppInfo{
+		Version: version,
+	}
+	return
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	response := Response{
-		Method: r.Method,
-		Path:   r.URL.Path,
+		AppInfo: appInfo(),
+		Method:  r.Method,
+		Path:    r.URL.Path,
 	}
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	json.NewEncoder(w).Encode(response)
